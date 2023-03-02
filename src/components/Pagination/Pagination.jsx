ba738b389359a52a99c9dafa1bb5dfd66529d0f3
Amr Elbahnsawy <73616484+amrbahnas@users.noreply.github.com>
2023-03-02 16:57:23 +0200

    pagination style

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -19,7 +19,7 @@ function Pagination({ postsPerPage, totalPosts, setCurrentPage, currentPage }) {
           <ul className="flex list-style-none">
             <li className="page-item">
               <button
-                className="page-link cursor-pointer relative block py-1.5 px-3  border-0 bg-transparent outline-none transition-all duration-300 rounded text-gray-800 hover:text-gray-800 focus:shadow-none"
+                className="page-link cursor-pointer relative block py-1.5 px-3  border-0 bg-transparent outline-none transition-all duration-300 rounded text-gray-800 dark:text-darkPText hover:text-gray-800 focus:shadow-none"
                 onClick={(e) => {
                   setCurrentPage((prev) => (prev > 1 ? prev - 1 : prev));
                 }}
@@ -32,8 +32,10 @@ function Pagination({ postsPerPage, totalPosts, setCurrentPage, currentPage }) {
                 <li key={number} className="cursor-pointer page-item">
                   <button
                     className={`${
-                      currentPage === number ? "bg-gray-200  text-gray-800" : ""
-                    }page-link relative block py-1.5 px-3 rounded border-0 bg-transparent outline-none transition-all duration-300  text-gray-800 hover:text-gray-800 hover:bg-gray-200  focus:shadow-none`}
+                      +currentPage === +number
+                        ? "bg-gray-200  text-gray-800 dark:bg-darkBody"
+                        : " bg-transparent"
+                    } page-link relative block py-1.5 px-3 rounded border-0  outline-none transition-all duration-300  text-gray-800 dark:text-darkPText  hover:text-gray-800 hover:bg-gray-200 dark:hover:bg-darkBody  focus:shadow-none`}
                     onClick={(e) => setCurrentPage(number)}
                   >
                     {number}
@@ -43,7 +45,7 @@ function Pagination({ postsPerPage, totalPosts, setCurrentPage, currentPage }) {
             })}
             <li className="page-item">
               <button
-                className="page-link cursor-pointer relative block py-1.5 px-3  border-0 bg-transparent outline-none transition-all duration-300 rounded text-gray-800  focus:shadow-none"
+                className="page-link cursor-pointer relative block py-1.5 px-3  border-0 bg-transparent outline-none transition-all duration-300 rounded text-gray-800 dark:text-darkPText  focus:shadow-none"
                 onClick={(e) =>
                   setCurrentPage((prev) =>
                     prev !== pageNumbers.length ? prev + 1 : prev
